662dcc32a6c13a153152c1e0a8d496eb2d31b6a9
S0us4Gl3b <glebsonsalmeida@gmail.com>
2024-12-07 02:41:04 -0300

    Add files via upload

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const playButton = document.getElementById('playButton');
     const playerDiv = document.getElementById('player');
 
+    const srcOriginal = "https://www.youtube.com/embed/${videoId}?autoplay=1"
+
     // Função para extrair o ID do vídeo do YouTube a partir da URL
     function extractVideoId(url) {
         const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
@@ -12,11 +14,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Função para embutir o player com o ID do vídeo
     function loadVideo(videoId) {
+        const videID2 = 'https://www.youtube.com/watch?v=QtXby3twMmI'
+        
+        
         playerDiv.innerHTML = `
             <iframe 
                 width="560" 
                 height="315" 
-                src="https://www.youtube.com/embed/${videoId}?autoplay=1" 
+                src= ${videID2}?autoplay=1
                 frameborder="0" 
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                 allowfullscreen>
