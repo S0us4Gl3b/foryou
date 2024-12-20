2503c3bcdfe88748a1bfdf4ed802bab40c2b75d7
S0us4Gl3b <glebsonsalmeida@gmail.com>
2024-12-19 21:50:21 -0300

    Add files via upload

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,84 +1,16 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const youtubeLinkInput = document.getElementById('youtubeLink');
-    const playButton = document.getElementById('playButton');
-    const playerDiv = document.getElementById('player');
-    const pauseButton = document.getElementById('playpause');
+document.addEventListener('DOMContentLoaded', () => {
 
-    let player // Variável para armazenar o player do YouTube
+    const videoLink = "https://www.youtube.com/watch?v=Br2rofxz_C0";
+    const videoContainer = document.getElementById('player');
 
+    // Extract the video ID from the URL
+    const urlRegex = /(?:https?:\/\/)?(?:www\.)?youtube\.com\/watch\?v=([\w-]+)|youtu\.be\/([\w-]+)/;
+    const match = videoLink.match(urlRegex);
 
-    let estadoBtnPlay = 0
-
-    const urlFixaParaVideo = 'https://www.youtube.com/watch?v=VPRjCeoBqrI'
-
-    // Função para extrair o ID do vídeo do YouTube a partir da URL
-    function extractVideoId(url) {
-        const regex = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
-        const match = url.match(regex);
-        return match ? match[1] : null;
+    if (match && (match[1] || match[2])) {
+        const videoId = match[1] || match[2];
+        videoContainer.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" allowfullscreen></iframe>`;
+    } else {
+        videoContainer.innerHTML = '<p style="color: red;">Invalid YouTube URL. Please try again.</p>';
     }
-
-    // Função chamada pela API do YouTube quando ela estiver pronta
-    window.onYouTubeIframeAPIReady = function () {
-        console.log("API do YouTube carregada.")
-
-    }
-
-    // Função para criar o player do YouTube
-    function createPlayer(videoId) {
-        playerDiv.innerHTML = ''; // Limpa o player anterior
-        player = new YT.Player('player', {
-            height: '315',
-            width: '560',
-            videoId: videoId,
-            playerVars: {
-                autoplay: 1,
-            },
-            events: {
-                onReady: function(event) {
-                    event.target.playVideo(); // Inicia o vídeo automaticamente quando o player estiver pronto
-                }
-            }
-        })
-    }
-
-    function iniciaVideo () {
-        const videoUrl = urlFixaParaVideo //youtubeLinkInput.value.trim();
-        const videoId = extractVideoId(videoUrl)
-
-        if (videoId) {
-            if (player) {
-                player.loadVideoById(videoId); // Recarrega o vídeo no player existente
-            } else {
-                createPlayer(videoId); // Cria o player se ele ainda não existir
-            }
-        } else {
-            alert('Por favor, insira um link válido do YouTube.')
-        }
-    }
-
-    // Evento de clique no botão para tocar o vídeo
-    playButton.addEventListener('click', () => {
-        iniciaVideo()
-    })
-
-    // Evento de clique no botão para pausar ou retomar o vídeo
-    pauseButton.addEventListener('click', () => {
-        if (player && player.getPlayerState) {
-            const playerState = player.getPlayerState();
-            if (playerState === YT.PlayerState.PLAYING) {
-                player.pauseVideo(); // Pausa o vídeo
-            } else if (playerState === YT.PlayerState.PAUSED) {
-                player.playVideo(); // Retoma o vídeo
-            }
-        } else {
-            alert('O player ainda não foi iniciado.')
-        }
-    })
-
-    // Adiciona o script da API do YouTube dinamicamente
-    const scriptTag = document.createElement('script')
-    scriptTag.src = "https://www.youtube.com/iframe_api"
-    const firstScriptTag = document.getElementsByTagName('script')[0]
-    firstScriptTag.parentNode.insertBefore(scriptTag, firstScriptTag)
 });
--- src/scripts/section-control.js
+++ src/scripts/section-control.js
@@ -1,43 +1,57 @@
 
 document.addEventListener('DOMContentLoaded', () => {
+    
+    const secCoracao = document.getElementById('sec-coracao')
+
     const container = document.getElementById('container');
-const coracaoDiv = document.getElementById('coracao-div');
+    const coracaoDiv = document.getElementById('coracao-div');
 
-function createHeartExplosion(x, y) {
-    // Para espalhar corações por toda a tela
-    for (let i = 0; i < 100; i++) {
-        const heart = document.createElement('div');
-        heart.classList.add('heart');
+    const divHome = document.getElementById('sec-home')
 
-        // Definir a posição inicial do coração para o centro do clique
-        heart.style.left = `${x - 10}px`;  // Ajuste para centralizar o coração
-        heart.style.top = `${y - 10}px`;   // Ajuste para centralizar o coração
+    const home = document.getElementsByClassName('home')
 
-        // Direção aleatória para explosão
-        const angle = Math.random() * 2 * Math.PI;
-        const distance = Math.random() * 800;  // Máxima distância para o qual o coração se moverá
-        const xOffset = Math.cos(angle) * distance;
-        const yOffset = Math.sin(angle) * distance;
-
-        // Definir as variáveis CSS para o movimento
-        heart.style.setProperty('--x', xOffset + 'px');  // Deslocamento em X
-        heart.style.setProperty('--y', yOffset + 'px');  // Deslocamento em Y
-
-        container.appendChild(heart);
-
-        // Remover o coração após a animação
-        heart.addEventListener('animationend', () => heart.remove());
-    }
+    function modificaDivDisplay () {
+        secCoracao.style.display = 'none';  // Esconde a seção do coração
+        divHome.style.display = 'flex'
     }
 
-    // Detectar clique do usuário em #coracao-div
-    coracaoDiv.addEventListener('click', (e) => {
-        // Obter as coordenadas do clique
-        const x = e.clientX;
-        const y = e.clientY;
+    function createHeartExplosion(x, y) {
+        // Para espalhar corações por toda a tela
+        for (let i = 0; i < 100; i++) {
+            const heart = document.createElement('div');
+            heart.classList.add('heart');
 
-        // Gerar a explosão de corações
-        createHeartExplosion(x, y);
-    });
+            // Definir a posição inicial do coração para o centro do clique
+            heart.style.left = `${x - 10}px`;  // Ajuste para centralizar o coração
+            heart.style.top = `${y - 10}px`;   // Ajuste para centralizar o coração
+
+            // Direção aleatória para explosão
+            const angle = Math.random() * 2 * Math.PI;
+            const distance = Math.random() * 800;  // Máxima distância para o qual o coração se moverá
+            const xOffset = Math.cos(angle) * distance;
+            const yOffset = Math.sin(angle) * distance;
+
+            // Definir as variáveis CSS para o movimento
+            heart.style.setProperty('--x', xOffset + 'px');  // Deslocamento em X
+            heart.style.setProperty('--y', yOffset + 'px');  // Deslocamento em Y
+
+            container.appendChild(heart);
+
+            // Remover o coração após a animação
+            heart.addEventListener('animationend', () => heart.remove());
+        }
+    }
+
+        // Detectar clique do usuário em #coracao-div
+        coracaoDiv.addEventListener('click', (e) => {
+            
+            // Obter as coordenadas do clique
+            const x = e.clientX;
+            const y = e.clientY;
+
+            // Gerar a explosão de corações
+            createHeartExplosion(x, y);
+            modificaDivDisplay()
+        });
 
 })
