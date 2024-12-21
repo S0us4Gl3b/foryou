e950c0e29275c95d3893df613b8136afe19e828c
S0us4Gl3b <glebsonsalmeida@gmail.com>
2024-12-21 09:57:21 -0300

    Add files via upload

--- src/scripts/player.js
+++ src/scripts/player.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-  
+    // Adicionar a API do YouTube dinamicamente
+    const tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
 
 
 
